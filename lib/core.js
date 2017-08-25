therootcompany/greenlock.js

commit 51739625176ff24fd09afc49e94eb4edb1327490

address #86 should be possible to wait for cert renewal to complete

lib/core.js
@@ -382,6 +382,7 @@ module.exports.create = function (le) {
       // Certificates
     , getAsync: function (args) {
         var copy = utils.merge(args, le);
+        var renewing;
         args = utils.tplCopy(copy);
 
         return core.certificates.checkAsync(args).then(function (certs) {
@@ -391,9 +392,14 @@ module.exports.create = function (le) {
           }
 
           if (core.certificates._isRenewable(args, certs)) {
-            certs._renewing = core.certificates.renewAsync(args, certs);
+            // it's time to renew the available cert
+            renewing = core.certificates.renewAsync(args, certs);
+            if (args.waitForRenewal) {
+              return renewing;
+            }
           }
 
+          // return existing unexpired (although potentially stale) certificates when available
           return certs;
         }).then(function (results) {
           // returns pems

commit dace8aea1fc8e0d53431d6ff313f32835951f01c

Change to address integration with greenlock-cli.
The integration was broken by 51739625176ff24fd09afc49e94eb4edb1327490,
primarily because the required property was incorrectly underscore-prefixed.
This fix restores it, unprefixed.

lib/core.js
@@ -382,7 +382,6 @@ module.exports.create = function (le) {
       // Certificates
     , getAsync: function (args) {
         var copy = utils.merge(args, le);
-        var renewing;
         args = utils.tplCopy(copy);
 
         return core.certificates.checkAsync(args).then(function (certs) {
@@ -393,13 +392,14 @@ module.exports.create = function (le) {
 
           if (core.certificates._isRenewable(args, certs)) {
             // it's time to renew the available cert
-            renewing = core.certificates.renewAsync(args, certs);
+            certs.renewing = core.certificates.renewAsync(args, certs);
             if (args.waitForRenewal) {
-              return renewing;
+              return certs.renewing;
             }
           }
 
           // return existing unexpired (although potentially stale) certificates when available
+          // there will be an additional .renewing property if the certs are being asynchronously renewed
           return certs;
         }).then(function (results) {
           // returns pems