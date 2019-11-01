b556643e781f4d45c26976d52334c5ad96ad5c12
AJ ONeal <coolaj86@gmail.com>
2019-11-01 05:30:49 -0600

    comments

--- bin/greenlock.js
+++ bin/greenlock.js
@@ -2,8 +2,12 @@
 'use strict';
 
 var args = process.argv.slice(2);
-console.log(args);
+//console.log(args);
+//['certonly', 'add', 'config', 'defaults', 'remove']
 if ('certonly' === args[0]) {
     require('./certonly.js');
     return;
 }
+
+console.error("command not yet implemented");
+process.exit();
