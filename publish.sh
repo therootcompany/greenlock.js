8384c165c780111922ae12197d179a5ace330c70
AJ ONeal <coolaj86@gmail.com>
2019-11-01 14:23:00 -0600

    fix

--- publish.sh
+++ publish.sh
@@ -10,6 +10,8 @@ git pull origin master
 git checkout npm
 git checkout master -- package.json
 git checkout master -- README.md
+git add package* README.md || true
+git commit -m "bump" || true
 sed -i '' -e 's|"name": ".root.greenlock"|"name": "greenlock"|' package.json
 npm install --save @root/greenlock@latest
 npm publish ./
